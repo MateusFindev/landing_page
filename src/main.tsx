dfeed4188969fad031fedde0cd9eb4e8bf212afd
MateusFinDev <finprogramador@gmail.com.br>
2024-08-12 22:14:15 -0300

    first class

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './styles/index.css'
+import Home from './pages/home.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Home></Home>
   </StrictMode>,
 )
---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import "../styles/utility.css"
+
+export default function Home()  {
+    return (
+        <>
+            <header className="container">
+
+                <ul className="even-columns">
+                    <li><a>Home</a></li>
+                    <li><a>Soluções</a></li>
+                    <li><a>Depoimentos</a></li>
+                    <li><a>Preços</a></li>
+                    <li><a>Contato</a></li>
+                </ul>
+            </header>
+        </>
+    )
+}
